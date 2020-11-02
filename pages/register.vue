85dff772af31e7aaa3d7edacd78d2b2a41df027c
tadashimasuda <tmasu4260@gmail.com>
2020-11-02 19:29:56 +0900

    create register form

--- pages/register.vue
+++ pages/register.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="container col-md-6 mt-5 shadow-lg py-5">
-        <h2 class="text-center text-success">ログイン</h2>
+        <h2 class="text-center text-success">新規登録</h2>
         <form>
+            <div class="form-group">
+                <label>ユーザーネーム</label>
+                <input type="text" class="form-control">
+                <small class="form-text text-danger">ユーザーネームがエラーです</small>
+            </div>
             <div class="form-group">
                 <label>メールアドレス</label>
                 <input type="email" class="form-control">
@@ -11,6 +16,11 @@
                 <label >パスワード</label>
                 <input type="password" class="form-control">
                 <small class="form-text text-danger">パスワードがエラーです</small>
+            </div>
+            <div class="form-group">
+                <label >パスワード(確認用)</label>
+                <input type="password" class="form-control">
+                <small class="form-text text-danger">パスワード(確認用)がエラーです</small>
             </div>
             <div class="text-center">
                <button type="submit" class="btn btn-primary btn-lg bg-success border-0">ログイン</button>
