bff73952ad7105f3b77d922ab6b7643c727b611c
tadashimasuda <tmasu4260@gmail.com>
2020-11-02 19:20:53 +0900

    create login form

---
+++ components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nuxt-link class="navbar-brand" to="/">Uber配達員メーター</nuxt-link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <nuxt-link class="nav-link" to="/">サンプル</nuxt-link>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <nuxt-link class="nav-link" to="/login">ログイン</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</nav>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container col-md-6 mt-5 shadow-lg py-5">
+        <h2 class="text-center">ログイン</h2>
+        <form>
+            <div class="form-group">
+                <label>Email address</label>
+                <input type="email" class="form-control">
+                <small class="form-text text-danger">メールアドレスがエラーです</small>
+            </div>
+            <div class="form-group">
+                <label >Password</label>
+                <input type="password" class="form-control">
+                <small class="form-text text-danger">パスワードがエラーです</small>
+            </div>
+            <div class="text-center">
+               <button type="submit" class="btn btn-primary btn-lg">ログイン</button>
+            </div>
+            <br>
+            <small class="d-block text-center">新規登録は<nuxt-link to="/register">こちら</nuxt-link></small>
+        </form>
+    </div>
+</template>
