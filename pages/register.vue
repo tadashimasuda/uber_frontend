1116196a13cff4f997a868c98e6b060bb09e5512
tadashimasuda <tmasu4260@gmail.com>
2020-11-02 22:22:42 +0900

    create register

--- pages/register.vue
+++ pages/register.vue
@@ -1,26 +1,34 @@
 <template>
     <div class="container col-md-6 mt-5 shadow-lg py-5">
         <h2 class="text-center text-success">新規登録</h2>
-        <form>
+        <form @submit.prevent="submit">
             <div class="form-group">
                 <label>ユーザーネーム</label>
-                <input type="text" class="form-control">
+                <input v-model.trim="form.name" type="text" class="form-control" autofocus>
                 <small class="form-text text-danger">ユーザーネームがエラーです</small>
             </div>
             <div class="form-group">
                 <label>メールアドレス</label>
-                <input type="email" class="form-control">
+                <input v-model.trim="form.email" type="email" class="form-control">
                 <small class="form-text text-danger">メールアドレスがエラーです</small>
             </div>
             <div class="form-group">
                 <label >パスワード</label>
-                <input type="password" class="form-control">
+                <input v-model.trim="form.password" type="password" class="form-control">
                 <small class="form-text text-danger">パスワードがエラーです</small>
             </div>
             <div class="form-group">
                 <label >パスワード(確認用)</label>
-                <input type="password" class="form-control">
+                <input v-model.trim="form.password_confirmation" type="password" class="form-control">
                 <small class="form-text text-danger">パスワード(確認用)がエラーです</small>
+            </div>
+            <div class="form-group">
+                <label>配達手段</label>
+                <select v-model="form.transport" class="form-control">
+                    <option value=1>自転車</option>
+                    <option value=2>バイク</option>
+                    <option value=3>車</option>
+                </select>
             </div>
             <div class="text-center">
                <button type="submit" class="btn btn-primary btn-lg bg-success border-0">ログイン</button>
@@ -30,3 +38,31 @@
         </form>
     </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            form:{
+                email:'',
+                name:'',
+                password:'',
+                password_confirmation:'',
+                transport:''
+            }
+        }
+    },
+    methods:{
+        async submit(){
+            await this.$axios.$post('register',this.form)
+            await this.$auth.loginWith('local',{
+                data:{
+                    email:this.form.email,
+                    password:this.form.password,
+                }
+            })
+            this.$router.push('/');
+        }
+    }
+}
+</script>
